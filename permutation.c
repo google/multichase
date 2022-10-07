google/multichase

commit 099b99d5f2211eef0b10758a598948d1feb67934

Poor performance with large stride values
The number of mixer indicies depends on the size of stride. There is a
nested loop like this in generate_chase_mixer:

  for (i = 0; i < NR_MIXERS; ++i) {
    gen_permutation(t, nr_mixer_indices, 0);
    for (j = 0; j < nr_mixer_indices; ++j)
      r[j * args->nr_mixers + i] = t[j];

When stride is large, this nested loop becomes very expensive, for
example with stride == 128K it takes: 1m24s to captue a single 0.5s
sample:

$ time ./multichase  -n 1 -vv  -s 128k
nr_threads = 1
page_size = 4096 bytes
total_memory = 268435456 (256.0 MiB)
stride = 131072
tlb_locality = 262144
chase = simple
generating permutation of 2048 elements (in 1024 TLB groups)
generating inverse permtuation
threading the chase (mixer_idx = 0)
samples (one column per thread, one row per sample):
 26.837  avg=26.837
 26.837

 real    1m24.120s
 user    1m22.947s
 sys     0m1.124s

Currently NR_MIXERS is set to 16K, and causes multichase to be too slow
when there are a lot of mixer indicies. Instead make this value dynamic,
by calculating the nearest power of two value to the total number of
parallel chases times the number of threads.

The result is following:
$ time ./multichase  -n 1 -vv  -s 128k
nr_threads = 1
page_size = 4096 bytes
total_memory = 268435456 (256.0 MiB)
stride = 131072
tlb_locality = 262144
chase = simple
nr_mixers = 2
generating permutation of 2048 elements (in 1024 TLB groups)
generating inverse permtuation
threading the chase (mixer_idx = 0)
samples (one column per thread, one row per sample):
 32.533  avg=32.533
 32.533

 real    0m1.108s
 user    0m1.024s
 sys     0m0.061s

Another benefit of smaller NR_MIXERS is that multichase uses less memory:
malloc(nr_mixer_indices * args->nr_mixers * sizeof(*r));

Signed-off-by: Pasha Tatashin <pasha.tatshin@soleen.com>

permutation.c
@@ -14,6 +14,7 @@
 #include "permutation.h"
 
 #include <assert.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -88,23 +89,26 @@ int is_a_permutation(const perm_t *perm, size_t nr_elts) {
   return 1;
 }
 
-// power-of-2 number of mixer permutations
-#define NR_MIXERS (16384)
-
-void generate_chase_mixer(struct generate_chase_common_args *args) {
+void generate_chase_mixer(struct generate_chase_common_args *args,
+                          size_t nr_mixers) {
   size_t nr_mixer_indices = args->nr_mixer_indices;
   void (*gen_permutation)(perm_t *, size_t, size_t) = args->gen_permutation;
 
+  /* Set number of mixers rounded up to the power of two */
+  args->nr_mixers = 1 << (CHAR_BIT * sizeof(long) -
+                          __builtin_clzl(nr_mixers - 1));
+  if (verbosity > 1)
+    printf("nr_mixers = %zu\n", args->nr_mixers);
   perm_t *t = malloc(nr_mixer_indices * sizeof(*t));
   if (t == NULL) {
     fprintf(stderr, "Could not allocate %lu bytes, check stride/memory size?\n",
             nr_mixer_indices * sizeof(*t));
     exit(1);
   }
-  perm_t *r = malloc(nr_mixer_indices * NR_MIXERS * sizeof(*r));
+  perm_t *r = malloc(nr_mixer_indices * args->nr_mixers * sizeof(*r));
   if (r == NULL) {
     fprintf(stderr, "Could not allocate %lu bytes, check stride/memory size?\n",
-            nr_mixer_indices * NR_MIXERS * sizeof(*r));
+            nr_mixer_indices * args->nr_mixers * sizeof(*r));
     exit(1);
   }
   size_t i;
@@ -112,10 +116,10 @@ void generate_chase_mixer(struct generate_chase_common_args *args) {
 
   // we arrange r in a transposed manner so that all of the
   // data for a particular mixer_idx is packed together.
-  for (i = 0; i < NR_MIXERS; ++i) {
+  for (i = 0; i < args->nr_mixers; ++i) {
     gen_permutation(t, nr_mixer_indices, 0);
     for (j = 0; j < nr_mixer_indices; ++j) {
-      r[j * NR_MIXERS + i] = t[j];
+      r[j * args->nr_mixers + i] = t[j];
     }
   }
   free(t);
@@ -130,7 +134,7 @@ void *generate_chase(const struct generate_chase_common_args *args,
   size_t stride = args->stride;
   size_t tlb_locality = args->tlb_locality;
   void (*gen_permutation)(perm_t *, size_t, size_t) = args->gen_permutation;
-  const perm_t *mixer = args->mixer + mixer_idx * NR_MIXERS;
+  const perm_t *mixer = args->mixer + mixer_idx * args->nr_mixers;
   size_t nr_mixer_indices = args->nr_mixer_indices;
 
   size_t nr_tlb_groups = total_memory / tlb_locality;
@@ -164,7 +168,7 @@ void *generate_chase(const struct generate_chase_common_args *args,
 
   dassert(is_a_permutation(perm_inverse, nr_elts));
 
-#define MIXED(x) ((x)*stride + mixer[(x) & (NR_MIXERS - 1)] * mixer_scale)
+#define MIXED(x) ((x)*stride + mixer[(x) & (args->nr_mixers - 1)] * mixer_scale)
 
   if (verbosity > 1)
     printf("threading the chase (mixer_idx = %zu)\n", mixer_idx);

multichase.c
@@ -757,7 +757,7 @@ int main(int argc, char **argv) {
 
   rng_init(1);
 
-  generate_chase_mixer(&genchase_args);
+  generate_chase_mixer(&genchase_args, nr_threads * chase->parallelism);
 
   // generate the chases by launching multiple threads
   genchase_args.arena =

multiload.c
@@ -1116,7 +1116,7 @@ int main(int argc, char **argv) {
   rng_init(1);
 
   if (run_test_type != RUN_BANDWIDTH) {
-    generate_chase_mixer(&genchase_args);
+    generate_chase_mixer(&genchase_args, nr_threads * chase->parallelism);
 
     // generate the chases by launching multiple threads
     if (verbosity > 2) printf("allocate genchase_args.arena\n");

permutation.h
@@ -49,6 +49,8 @@ struct generate_chase_common_args {
   size_t stride;        // size of each element
   size_t tlb_locality;  // group accesses within this range in order to
                         // amortize TLB fills
+  size_t nr_mixers;     // Rounded up to power of two number of mixers:
+                        // nr_threads * parallelism rounded up to power of two
   void (*gen_permutation)(perm_t *, size_t,
                           size_t);  // function for generating
                                     // permutations
@@ -59,7 +61,8 @@ struct generate_chase_common_args {
 };
 
 // create the mixer table
-void generate_chase_mixer(struct generate_chase_common_args *args);
+void generate_chase_mixer(struct generate_chase_common_args *args,
+                          size_t nr_mixers);
 
 // create a chase for the given mixer_idx and return its first pointer
 void *generate_chase(const struct generate_chase_common_args *args,

commit 44146d4099f87098d901ff242f99f33e0490a96b

fix build errors on NR_MIXERS
Due to changes introduced by:
 099b99d5f221 ("Poor performance with large stride values")

Signed-off-by: Stephane Eranian <eranian@google.com>

permutation.c
@@ -200,7 +200,7 @@ void *generate_chase_long(const struct generate_chase_common_args *args,
   void (*gen_permutation)(perm_t *, size_t, size_t) = args->gen_permutation;
   size_t nr_mixer_indices = args->nr_mixer_indices;
   size_t nr_iteration = nr_mixer_indices / total_par;
-  const perm_t *mixer = args->mixer + mixer_idx * nr_iteration * NR_MIXERS;
+  const perm_t *mixer = args->mixer + mixer_idx * nr_iteration * args->nr_mixers;
 
   size_t nr_tlb_groups = total_memory / tlb_locality;
   size_t nr_elts_per_tlb = tlb_locality / stride;
@@ -265,7 +265,7 @@ void *generate_chase_long(const struct generate_chase_common_args *args,
 
 // Get the [(x mod NR_MIXER)th element in the jth row of mixer]th element
 // in the xth stride of the array.
-#define MIXED_2(x,j) ((x)*stride + (mixer + j*NR_MIXERS)[(x) & (NR_MIXERS-1)] * mixer_scale)
+#define MIXED_2(x,j,n) ((x)*stride + (mixer + j*n)[(x) & (n-1)] * mixer_scale)
 
   if (verbosity > 1)
     printf("threading the chase (mixer_idx = %zu)\n", mixer_idx);
@@ -275,7 +275,7 @@ void *generate_chase_long(const struct generate_chase_common_args *args,
   for (i = 0; i < nr_elts * nr_iteration; ++i) {
     size_t next;
     dassert(perm[perm_inverse[i]] == i);
-    assert(*(void **)(arena + MIXED_2(i%nr_elts,i/nr_elts)) == NULL);
+    assert(*(void **)(arena + MIXED_2(i%nr_elts,i/nr_elts, args->nr_mixers)) == NULL);
     next = perm_inverse[i] + 1;
     // If next is the position representing the start of a new iteration of
     // permutation, set next to be the position representing the start of
@@ -288,16 +288,16 @@ void *generate_chase_long(const struct generate_chase_common_args *args,
       // new position is the start of next iteration.
       size_t new = (i/nr_elts + 1) * nr_elts;
       new = (new == nr_iteration * nr_elts) ? 0 : new;
-      *(void **)(arena + MIXED_2(i%nr_elts,i/nr_elts)) =
-        (void *)(arena + MIXED_2(new%nr_elts,new/nr_elts));
+      *(void **)(arena + MIXED_2(i%nr_elts,i/nr_elts, args->nr_mixers)) =
+        (void *)(arena + MIXED_2(new%nr_elts,new/nr_elts, args->nr_mixers));
     } else {
-      *(void **)(arena + MIXED_2(i%nr_elts,i/nr_elts)) =
-        (void *)(arena + MIXED_2(perm[next]%nr_elts,perm[next]/nr_elts));
+      *(void **)(arena + MIXED_2(i%nr_elts,i/nr_elts, args->nr_mixers)) =
+        (void *)(arena + MIXED_2(perm[next]%nr_elts,perm[next]/nr_elts, args->nr_mixers));
     }
   }
 
   free(perm);
   free(perm_inverse);
 
-  return arena + MIXED_2(0,0);
+  return arena + MIXED_2(0,0, args->nr_mixers);
 }